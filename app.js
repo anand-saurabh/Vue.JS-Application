e04507cc1343d857c446a156082223a89fe6c9fe
anand-saurabh <saurabh.anand@ufl.edu>
2020-06-05 17:31:40 -0400

    implemented v-model usage in project to bind data

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ new Vue({
   {
     name: 'Users',
     job: 'Software',
+    count: 0,
     website: "https://vuejs.org/v2/guide/"
   },
   methods: {
     greet: function()
     {
       return "Welcome" + " " + this.name + " This is my first application in Vue.js :" + " follow the link below to read view documentation";
+    },
+    getlikes : function ()
+    {
+      this.count++;
+    },
+    click : function()
+    {
+      alert("you clicked");
     }
   }
 });
